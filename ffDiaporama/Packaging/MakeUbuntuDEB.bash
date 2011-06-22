fd3e1d5dd4a93b7b96068091f5f9e044839d0282
domledom <domledom@f803d417-eb43-4c80-8848-1a7435ce69a7>
2011-06-22 18:14:48 +0000

    Packaging UBUNTU+renomage des fichiers background
    git-svn-id: file:///media/werkstatt/064f12d4-5f27-4839-9537-9ffb7939d236/ffDiaporama-Entwicklung-2023/ffdiaporama-svn/trunk@76 f803d417-eb43-4c80-8848-1a7435ce69a7

--- ffDiaporama/Packaging/MakeUbuntuDEB.bash
+++ ffDiaporama/Packaging/MakeUbuntuDEB.bash
@@ -8,12 +8,13 @@ wget -O ffDiaporama.tar.gz http://svn.tuxfamily.org/viewvc.cgi/ffdiaporama_trunk
 tar -xzf ffDiaporama.tar.gz
 rm ffDiaporama.tar.gz
 cd ffdiaporama_trunk
-cp ffDiaporama/Packaging/debian ~/debbuild/SOURCES/ffdiaporama-0.99.2
-rm -Rf ffDiaporama/Packaging
 rm ffDiaporama/locale/*.ts
+mv ffDiaporama/Packaging/debian ffDiaporama/debian
+rm -Rf ffDiaporama/Packaging
 mv ffDiaporama ffdiaporama-0.99.2
 tar -czf ../ffdiaporama_0.99.2.orig.tar.gz *
 mv ffdiaporama-0.99.2 ~/debbuild/SOURCES/ffdiaporama-0.99.2
-cd ffdiaporama-0.99.2
+cd ../ffdiaporama-0.99.2
 debuild -b --lintian-opts -i
+
 
