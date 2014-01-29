7c6ab5524d01fa6053e59cba7f71ddd0ea622d6a
domledom <domledom@f803d417-eb43-4c80-8848-1a7435ce69a7>
2014-01-29 15:49:28 +0000

    add setup.bash script
    git-svn-id: file:///media/werkstatt/064f12d4-5f27-4839-9537-9ffb7939d236/ffDiaporama-Entwicklung-2023/ffdiaporama-svn/trunk@463 f803d417-eb43-4c80-8848-1a7435ce69a7

---
+++ ffDiaporama_openclipart/setup.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+fi
+
+#==============================================================================
+
+if ! wget -t 0 http://ftp.osuosl.org/pub/openclipart/downloads/0.18/openclipart-0.18-svgonly.zip -O openclipart-0.18-svgonly.zip; then
+    echo Impossible to get ffDiaporama-openclipart from ffDiaporama Web site
+    cd ~
+    exit 1
+else
+    unzip openclipart-0.18-svgonly.zip -d /usr/share/ffDiaporama/clipart/openclipart-0.18-svgonly/
+fi
+
+#==============================================================================
+
+cp openclipart-0.18-svgonly-licence.txt /usr/share/ffDiaporama/clipart/openclipart-0.18-svgonly/
+cp openclipart-0.18-svgonly-readme.txt /usr/share/ffDiaporama/clipart/openclipart-0.18-svgonly/
